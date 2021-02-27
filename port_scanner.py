bc7f4007ab42a3504e6c9078b3d8b07d44877e27
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-27 10:09:58 -0800

    need to set timeout

--- port_scanner.py
+++ port_scanner.py
@@ -1,36 +1,19 @@
 import socket
-    
+
 def get_open_ports(target, port_range, verbose=None):
     open_ports = []
-    
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     start, end = port_range
     
-    def port_scan(port):
-        try:
-            s.connect((target, port))
-            return True
-        except: 
-            return False
-        # try:
-        #     s.connect((target, port))
-        #     return True
-        # except:
-        #     return False
-    
     for i in range(start, (end+1)):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         port = i
-        if port_scan(port):
+        result = s.connect_ex((target, port))
+        if result == 0:
             print(f"The port {port} is Open.")
             open_ports.append(port)
         else:
             print(f'The port {port} is Closed.')
-            
-    # if verbose == True:
-        # return descriptive string of open ports: 
-        
-        # Open ports for {URL} ({IP address})
-        # PORT     SERVICE
-        # {port}   {service name}
+        s.close()
 
     return(open_ports)
